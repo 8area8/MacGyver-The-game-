9339b095d891f56b48012ba71a8d8d4ffb116adb
8area8 <mbriolet.ma@gmail.com>
2018-06-09 16:11:12 +0200

    building UML / building the core structure.

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 """Launch the game from this file."""
+
+# imports
+
+
+def main():
+    """Launch the game."""
+    pass
+
+    # Initialisation de pygame
+
+    # importation des images
+    # Initialisation des variales principales
+    interface = None
+    running = True
+
+    while(running):
+        pass
+
+        # vérification de interface
+
+        # events
+
+        # update
+
+        # refresh
+
+        # FPS
+
+
+if __name__ == "__main__":
+    main()
